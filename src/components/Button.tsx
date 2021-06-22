96685c36bbb06935113feef2e67b4127c492e984
Robson <robsonbrazjunior@gmail.com>
2021-06-22 18:24:15 -0300

    Style for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,10 @@
 import { ButtonHTMLAttributes } from 'react'
+import '../styles/button.scss';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button(props: ButtonProps) {
     return (
-        <button className="button"></button>
+        <button className="button" {...props} />
     )
 }

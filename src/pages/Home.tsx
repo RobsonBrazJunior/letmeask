952ecb0badc1621afa4f1211196ec045c5fb503d
Robson <robsonbrazjunior@gmail.com>
2021-06-21 22:52:52 -0300

    Add style sheets

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,24 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import '../styles/auth.scss'
+
 export function Home() {
     return (
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Illustration that symbolizes questions and answers" />
                 <strong>Create live Q&amp;A rooms</strong>
                 <p>Answer your audience's questions in real time</p>
             </aside>
             <main>
-                <div>
+                <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button>
+                    <button className="create-room">
                         <img src={googleIconImg} alt="Google's logo" />
                         Create your room with Google
                     </button>
-                    <div>Or entry in a room</div>
+                    <div className="separator">Or entry in a room</div>
                     <form>
                         <input
                             type="text"

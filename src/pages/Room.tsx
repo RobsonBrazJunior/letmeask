e035c88ab1daa33a23b11113c8398882ced74a13
Robson <robsonbrazjunior@gmail.com>
2021-06-26 18:01:15 -0300

    Create basic structure for Room page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,33 @@
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+
 export function Room() {
     return (
-        <h1>Room</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <div>code</div>
+                </div>
+            </header>
+
+            <div className="content">
+                <div className="room-title">
+                    <h1>React Room</h1>
+                    <span>4 Questions</span>
+                </div>
+
+                <form>
+                    <textarea
+                        placeholder="What would you like to ask?"
+                    />
+
+                    <div className="form-footer">
+                        <span>To submit a question, <button>please login</button>.</span>
+                        <Button type="submit">Submit a question</Button>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }

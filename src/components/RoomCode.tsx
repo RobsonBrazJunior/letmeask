7dd5d3f8c4c7ccf443a4eac041a72b0ad2a2a992
Robson <robsonbrazjunior@gmail.com>
2021-07-05 18:48:38 -0300

    Fix code of the room

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -16,7 +16,7 @@ export function RoomCode(props: RoomCodeProps) {
             <div>
                 <img src={copyImg} alt="Copy Room Code" />
             </div>
-            <span>Room #idroom</span>
+            <span>Room #{props.code}</span>
         </button>
     )
 }
